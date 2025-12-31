bfa4cbd0d4266ba4c1cecf3b2972dcc70b0323cf
yuta asakura <yuta.asakura.se@gmail.com>
2025-12-31 15:37:13 +0800

    Add onSelectItem prop to ListGroup for item selection handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -17,7 +18,10 @@ function ListGroup({ items, heading }: Props) {
           <li
             className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
             key={item}
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
