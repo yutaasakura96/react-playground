9166b0e81e79f1e4b4342f4a22aaa451a6f7294c
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-01 23:23:42 +0800

    Add ExpandableText component to App; enhance UI with additional content display

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
+import { useState } from 'react';
+import Alert from './components/Alert';
+import Button from './components/Button/Button';
+import ExpandableText from './components/ExpandableText';
 function App() {
   const [alertVisibility, setAlertVisibility] = useState(false);
   return (
     <div>
       {alertVisibility && <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>}
       <Button onClick={() => setAlertVisibility(true)}>Click me</Button>
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta vero facilis temporibus iure
+        sit aspernatur excepturi, quo recusandae consequatur reiciendis odio eos repudiandae iusto
+        nihil suscipit cupiditate, voluptatem vitae. Quaerat?
+      </ExpandableText>
     </div>
   );
 }
