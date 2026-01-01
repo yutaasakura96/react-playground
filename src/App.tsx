da3c1b8962ce5c25c4526439e7b394bfe9f18a76
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-01 10:00:22 +0800

    Replace ListGroup with Alert and Button components in App; update UI to display a greeting and a clickable button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import ListGroup from './components/ListGroup'
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
   return (
-    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-  )
+    <div>
+      <Alert>Hello World</Alert>
+      <Button onClick={() => console.log('Clicked')}>Click me</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
