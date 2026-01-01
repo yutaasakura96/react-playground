d2cdbe4b3ccf2e43af2ece7414cf2c1cf4c304dc
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-01 21:48:59 +0800

    Refactor import paths in App component; change Alert component to use type imports; remove unused ProductList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 function App() {
   const [alertVisibility, setAlertVisibility] = useState(false);
   return (
