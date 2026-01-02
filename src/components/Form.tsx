26be4b926a2858915f758c659262be6493fa1f48
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-02 15:01:27 +0800

    Integrate react-hook-form into Form component for improved form handling; update package.json and package-lock.json to include react-hook-form dependency

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,39 +1,24 @@
-import { useState, type FormEvent } from 'react';
-
+import { useForm, type FieldValues } from 'react-hook-form';
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: '',
-    age: 0,
-  });
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input
-          onChange={(event) => setPerson({ ...person, name: event.target.value })}
-          value={person.name}
-          type="text"
-          className="form-control"
-          id="name"
-        />
+        <input {...register('name')} type="text" className="form-control" id="name" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input
-          onChange={(event) => setPerson({ ...person, age: parseInt(event.target.value) })}
-          value={person.age}
-          type="number"
-          className="form-control"
-          id="age"
-        />
+        <input {...register('age')} type="number" className="form-control" id="age" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
