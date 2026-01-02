9d790645a6d671c062806568d5bdf94196569ad3
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-02 14:36:13 +0800

    Enhance Form component to utilize refs for input handling; update handleSubmit to log user input for name and age

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,32 @@
-import type { FormEvent } from 'react';
+import { useRef, type FormEvent } from 'react';
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: '', age: 0 };
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log('submitted');
+    if (nameRef.current !== null) {
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      person.age = parseInt(ageRef.current.value);
+    }
+    console.log(person);
   };
-
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input type="text" className="form-control" id="name" />
+        <input ref={nameRef} type="text" className="form-control" id="name" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input type="number" className="form-control" id="age" />
+        <input ref={ageRef} type="number" className="form-control" id="age" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
