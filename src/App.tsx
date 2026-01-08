a8a82e7f50390e5c7850d5208654e94147c1a4a7
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-08 23:45:56 +0800

    Add axios for API calls in App component; update package.json and package-lock.json to include axios dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import Form from './components/Form';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+}
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users').then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
-    <div>
-      <Form />
-    </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
