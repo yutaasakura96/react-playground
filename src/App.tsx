2d52f5a248642b6b46d3c908d1ec376fb535840b
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-09 00:18:17 +0800

    Add loading state to App component; display spinner while fetching user data

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,22 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsLoading(true);
     axios
       .get<User[]>('https://jsonplaceholder.typicode.com/users', { signal: controller.signal })
       .then((response) => {
         setUsers(response.data);
+        setIsLoading(false);
       })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
     return () => controller.abort();
   }, []);
@@ -26,6 +31,7 @@ function App() {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
