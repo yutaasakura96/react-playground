856e296b4e851db23f76c84da6b3c2683cfa5a22
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-08 23:54:18 +0800

    Implement error handling in App component; display error message when API call fails

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,28 @@ interface User {
 }
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users').then((response) => {
-      setUsers(response.data);
-    });
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
   }, []);
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
