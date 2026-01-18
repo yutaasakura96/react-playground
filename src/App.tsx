50c4e321845b7aa6035399f4080ac0426c1dd5c6
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-18 20:54:06 +0900

    Refactor App component to remove user management logic; simplify structure by replacing with a static welcome message and removing unused hooks and services. Clean up CSS styles and add new routing components for improved navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,7 @@
-import useUsers from './hooks/useUsers';
-import userService, { type User } from './services/user-service';
+import './App.css';
 
 function App() {
-  const { users, error, isLoading, setError, setUsers } = useUsers();
-  const deleteUser = (user: User) => {
-    const originalUsers = [...users];
-    setUsers(users.filter((u) => u.id !== user.id));
-
-    userService.delete(user.id).catch((error) => {
-      setError(error.message);
-      setUsers(originalUsers);
-    });
-  };
-
-  const addUser = () => {
-    const originalUsers = [...users];
-    const newUser = { id: 0, name: 'John' };
-    setUsers([newUser, ...users]);
-    userService
-      .create(newUser)
-      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
-      .catch((error) => {
-        setError(error.message);
-        setUsers(originalUsers);
-      });
-  };
-
-  const updateUser = (user: User) => {
-    const originalUsers = [...users];
-    const updatedUser = { ...user, name: user.name + '!' };
-    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
-    userService.update(updatedUser).catch((error) => {
-      setError(error.message);
-      setUsers(originalUsers);
-    });
-  };
-
-  return (
-    <>
-      {error && <p className="text-danger">{error}</p>}
-      {isLoading && <div className="spinner-border"></div>}
-      <button className="btn btn-primary mb-3" onClick={addUser}>
-        Add User
-      </button>
-      <ul className="list-group ">
-        {users.map((user) => (
-          <li key={user.id} className="list-group-item d-flex justify-content-between">
-            {user.name}
-            <div>
-              <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>
-                Update
-              </button>
-              <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <h1>React Starter Project</h1>;
 }
 
 export default App;
