e55961feee2295cbda0230bdbf59a91eb88eb8f9
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-18 19:25:28 +0900

    Integrate React Query for data fetching in App component; add QueryClient and ReactQueryDevtools for improved state management and debugging capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
