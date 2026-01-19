2f961128bc9a18c99e1a6f9362fc5137c20409e6
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-19 16:42:20 +0900

    Refactor TodoForm component to implement optimistic updates and error handling during todo addition, improving user experience by maintaining previous state on error.

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,13 +3,27 @@ import axios from 'axios';
 import { useRef } from 'react';
 import type { Todo } from '../hooks/useTodos';
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) => axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
       .then(res => res.data),
-    onSuccess: (savedTodo) => {
-      queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])]);
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todos']) || [];
+      queryClient.setQueryData<Todo[]>(['todos'], todos => [newTodo, ...(todos || [])]);
+      if (ref.current) ref.current.value = '';
+      return { previousTodos };
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(['todos'], todos => todos?.map(todo => todo === newTodo ? savedTodo : todo));
+    },
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(['todos'], context?.previousTodos);
     }
   })
   const ref = useRef<HTMLInputElement>(null);
