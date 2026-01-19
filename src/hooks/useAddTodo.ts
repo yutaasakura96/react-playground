2b1104bf1eb21352efa2987e47d6442f20961c87
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-19 17:34:07 +0900

    Refactor useAddTodo hook to utilize APIClient for posting todos, improving modularity and consistency in data management.

--- src/hooks/useAddTodo.ts
+++ src/hooks/useAddTodo.ts
@@ -1,8 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
 import { CACHE_KEY_TODOS } from '../react-query/constants';
+import APIClient from '../services/apiClient';
 import type { Todo } from './useTodos';
 
+const apiClient = new APIClient<Todo>('/todos');
 interface AddTodoContext {
   previousTodos: Todo[];
 }
@@ -10,8 +11,7 @@ interface AddTodoContext {
 const useAddTodo = (onAdd: () => void) => {
   const queryClient = useQueryClient();
   return useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: (todo: Todo) => axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
-      .then(res => res.data),
+    mutationFn: apiClient.post,
     onMutate: (newTodo: Todo) => {
       const previousTodos = queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
       queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [newTodo, ...todos]);
