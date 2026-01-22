e4fa8955028e955c7d1c836604969bdf784c79fb
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-22 15:04:26 +0900

    Integrate Zustand for counter state management by replacing useReducer with a custom store; update NavBar and Counter components to reflect the new state management approach, enhancing modularity and performance.

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,11 @@
-import { useContext } from 'react';
 import LoginStatus from './auth/LoginStatus';
-import TaskContext from './tasks/tasksContext';
+import useCounterStore from './counter/store';
 
 const NavBar = () => {
-  const { tasks } = useContext(TaskContext);
+  const { counter } = useCounterStore();
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
