23b6b185333d77de1811bfbb64f2a741fdee821d
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-22 10:29:37 +0900

    Refactor LoginStatus component to utilize custom useAuth hook for improved context management; add useAuth hook to encapsulate AuthContext logic, enhancing modularity and readability.

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,8 +1,7 @@
-import { useContext } from 'react';
-import AuthContext from './context/authContext';
+import useAuth from './hooks/useAuth';
 
 const LoginStatus = () => {
-  const { user, dispatch } = useContext(AuthContext);
+  const { user, dispatch } = useAuth();
 
   if (user)
     return (
