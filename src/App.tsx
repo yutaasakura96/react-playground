0f2c8075ba46275a86b305471169db57796e0322
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-22 15:23:54 +0900

    Refactor authentication management by removing AuthProvider and related components; update LoginStatus to utilize Zustand for user state management, enhancing modularity and simplifying the authentication flow.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import AuthProvider from './state-management/auth/AuthProvider';
 import Counter from './state-management/counter/Counter';
 import HomePage from './state-management/HomePage';
 import NavBar from './state-management/NavBar';
@@ -7,13 +6,13 @@ import { TaskProvider } from './state-management/tasks';
 
 function App() {
   return (
-    <AuthProvider>
-      <TaskProvider>
-        <Counter />
-        <NavBar />
-        <HomePage />
-      </TaskProvider>
-    </AuthProvider>
+
+    <TaskProvider>
+      <Counter />
+      <NavBar />
+      <HomePage />
+    </TaskProvider>
+
   )
 }
 
