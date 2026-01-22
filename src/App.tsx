17eb1adf91f0698b88eaea208fa5f03d913499f0
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-22 10:26:46 +0900

    Refactor App component to integrate AuthProvider for user authentication, replacing AuthContext; streamline state management by removing authReducer and enhancing modularity in the application structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import { useReducer } from 'react';
 import './App.css';
-import AuthContext from './state-management/context/authContext';
+import AuthProvider from './state-management/AuthProvider';
 import TaskContext from './state-management/context/tasksContext';
 import HomePage from './state-management/HomePage';
 import NavBar from './state-management/NavBar';
-import authReducer from './state-management/reducers/authReducer';
 import tasksReducer from './state-management/reducers/tasksReducer';
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, '');
+
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TaskContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TaskContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   )
 }
 
