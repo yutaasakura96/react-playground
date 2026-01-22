7d5dfa52f668c5966d7226a26dbe0348ef38e5ba
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-22 23:21:56 +0900

    Update package dependencies to include react-router-dom for improved routing capabilities; refactor main entry point to utilize RouterProvider for enhanced navigation structure.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import router from './routing/routes.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
