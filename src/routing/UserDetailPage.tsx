96d34b5050fd6938f6f40a6b35df13b03960d0a9
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-23 09:57:04 +0900

    Implement dynamic user detail display in UserDetailPage; utilize react-router hooks for accessing URL parameters and search parameters, enhancing user experience with personalized content.

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,12 @@
+import { useLocation, useParams, useSearchParams } from 'react-router-dom';
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(location);
+  console.log(searchParams, setSearchParams);
+  return <p>User {params.id}</p>;
 };
 
 export default UserDetailPage;
